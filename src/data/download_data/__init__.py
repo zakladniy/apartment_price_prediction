599cd9231d8ab568c5d05e9e4c0edf3ba27fb715
Anton Zakladniy <anton.zakladniy@biovitrum.ru>
2022-04-19 21:23:31 +0300

    Add scripts for data extract

--- src/data/download_data/__init__.py
+++ src/data/download_data/__init__.py
@@ -1,4 +1,4 @@
-from download_data.cian_api import (
+from cian_api import (
     get_all_apartment_info_from_json,
     get_raw_data_from_api,
     nested_check,
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,16 +1,18 @@
-# -*- coding: utf-8 -*-
-import click
+"""Create raw dataset from outer source."""
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+
+import click
 
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Create and preprocess dataset from outer source.
+
+    @param input_filepath:
+    @param output_filepath:
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
